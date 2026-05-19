7f2206530caa73e1cb313a2617c94869b77c83b6
jibinpv <thamanna.aslam.07@gmail.com>
2026-05-19 14:59:59 +0530

    Added Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  }
+})
